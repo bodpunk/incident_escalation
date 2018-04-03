3afb1eabc3c9c89c4a9776b81c4b9ddb5c643ddf
bodpunk <bodpunk@mail.ru>
2018-04-04 00:51:51 +0500

    Text-block + fix
    Added textarea + fixed bug for click on first element

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
-    $('.main-tree__element').click(function() {
+    $('.main-tree__element:not(.main-tree__first-element)').click(function() {
       $(this).siblings().removeClass('selected');
       $(this).siblings().removeClass('line__visible');
       $(this).addClass('selected');
       var identificator = $(this).attr('id');
+      var text = $(this).text();
       $('.' + identificator + '__inner').addClass('line__visible');
       $('.main-tree__line:not(.line__visible) span').removeClass('selected');
+      $('.textarea').append('&#13;&#10;' + text);
     });
 });
