f359a1e04e45a01b686bdd4fa3bc92e5e94ad1ec
bodpunk <bodpunk@mail.ru>
2018-04-05 23:00:57 +0500

    Added copy-button and other
    Added copy-button, js for it; changed width (30% for textarea, 70% for tree); comments for js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
-    $('.main-tree__element:not(.main-tree__first-element)').click(function() {
-      $(this).siblings().removeClass('selected');
-      $(this).siblings().removeClass('line__visible');
-      $(this).addClass('selected');
-      var identificator = $(this).attr('id');
-      var text = $(this).text();
-      $('.' + identificator + '__inner').addClass('line__visible');
-      $('.main-tree__line:not(.line__visible) span').removeClass('selected');
-      $('.textarea').append('&#13;&#10;' + text);
+    $('.main-tree__element:not(.main-tree__first-element)').click(function() { // Ловим нажатие на любой элемент дерева, за исключением первого
+      $(this).siblings().removeClass('selected'); // Снимаем класс выделения (и само выделение) с соседей нажатого элемента
+      $(this).siblings().removeClass('line__visible'); //Снимаем класс раскрытия ветви (и саму ветвь) с соседей нажатого элемента
+      $(this).addClass('selected'); // повесили класс выделения на нажатый элемент
+      var identificator = $(this).attr('id'); // задаем переменную, которая равна идентификатору нажатого элемента
+      var text = $(this).text(); //задаем переменную, которая равна тексту нажатого элемента
+      $('.' + identificator + '__inner').addClass('line__visible'); // раскрываем ветвь нажатого элемента
+      $('.main-tree__line:not(.line__visible) span').removeClass('selected'); // скрываем выделение элементов на скрытых ветках
+      $('.textarea-block__text').append('&#13;&#10;' + text); // добавляем в текстовое поле содержимое нажатого элемента (&#13;&#10; = <br> for textarea)
+    });
+    $('.textarea-block__button').click(function() { // Ловим нажатие по кнопке "Копировать"
+      $('.textarea-block__text').select(); // Выделяем содержимое textarea
+      document.execCommand("copy"); // Копируем содержимое textarea
+      $('.textarea-block__button').focus(); // Снимаем выделение textarea, установив фокус на кнопку
     });
 });
