6a5ed0e7bb1d6dd22eabb6382d511c7bc7982aa1
bodpunk <bodpunk@mail.ru>
2018-04-06 00:35:26 +0500

    Fixed copying from elements to textarea

--- js/main.js
+++ js/main.js
@@ -7,7 +7,10 @@ $(document).ready(function() {
       var text = $(this).text(); //задаем переменную, которая равна тексту нажатого элемента
       $('.' + identificator + '__inner').addClass('line__visible'); // раскрываем ветвь нажатого элемента
       $('.main-tree__line:not(.line__visible) span').removeClass('selected'); // скрываем выделение элементов на скрытых ветках
-      $('.textarea-block__text').append('&#13;&#10;' + text); // добавляем в текстовое поле содержимое нажатого элемента (&#13;&#10; = <br> for textarea)
+      $('.textarea-block__text').text('В чём проблема?'); // Обнуляем содержимое textarea, сразу прописывая в него "В чём проблема?"
+      $('.selected').each(function() { // для каждого элемента с классом .selected
+        $('.textarea-block__text').append('&#13;&#10;' + $(this).text()); // добавляем его содержимое в textarea в новую строку
+      });
     });
     $('.textarea-block__button').click(function() { // Ловим нажатие по кнопке "Копировать"
       $('.textarea-block__text').select(); // Выделяем содержимое textarea
