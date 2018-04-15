980d0c0a7ae6bba2014ff99e5cb15e651dbf8508
bodpunk <bodpunk@mail.ru>
2018-04-15 22:36:30 +0500

    MiniFix

--- js/main.js
+++ js/main.js
@@ -7,9 +7,9 @@ $(document).ready(function() {
       var text = $(this).text(); //задаем переменную, которая равна тексту нажатого элемента
       $('.' + identificator + '__inner').addClass('line__visible'); // раскрываем ветвь нажатого элемента
       $('.main-tree__line:not(.line__visible) span').removeClass('selected'); // скрываем выделение элементов на скрытых ветках
-      $('.textarea-block__text').text('В чём проблема?'); // Обнуляем содержимое textarea, сразу прописывая в него "В чём проблема?"
+      $('.textarea-block__text').text(''); // Обнуляем содержимое textarea
       $('.selected').each(function() { // для каждого элемента с классом .selected
-        $('.textarea-block__text').append('&#13;&#10;&#13;&#10;' + $(this).text()); // добавляем его содержимое в textarea в новую строку
+        $('.textarea-block__text').append($(this).text() + '&#13;&#10;&#13;&#10;'); // добавляем его содержимое в textarea в новую строку
       });
       $('.textarea-block__text').val($('.textarea-block__text').text()); // значение textarea = текст textarea (на случай введенных комментариев и последующего изменения элементов selected)
     });
